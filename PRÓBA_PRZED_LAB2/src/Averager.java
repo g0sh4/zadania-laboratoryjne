16eb859f0cf751f5eebb1a9eb2fff9ccf705d8ee
Małgorzata Olech <01159108@pw.edu.pl>
2021-03-14 19:37:54 +0100

    lab2_próba_obsługi_repozytorium_poprawka2

--- PRÓBA_PRZED_LAB2/src/Averager.java
+++ PRÓBA_PRZED_LAB2/src/Averager.java
@@ -1,2 +1,20 @@
 public class Averager {
+    public static void main(String[] args){
+        double[] vals = new double[]{1.5, 3.0, 5.2};
+
+
+        double suma = 0;
+        for(int i=0; i<vals.length; i++){
+            suma = suma + vals[i];
+
+        }
+        System.out.println("suma = " +suma);
+        System.out.println("średnia= "+suma/vals.length);
+
+
+    }
+
+
+
+
 }
